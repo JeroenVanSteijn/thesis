bf226e74be22d98d4cac4ccc800dd9eb68e4c767
Jeroen van Steijn <jeroen@bytecode.nl>
2022-06-14 14:35:36 +0200

    WIP

--- uncertain-weights/main.py
+++ uncertain-weights/main.py
@@ -4,6 +4,7 @@ from torch import optim
 import numpy as np
 from SPO_learner import SGD_SPO_dp_lr
 from MSE_learner import MSE_Learner
+import matplotlib.pyplot as plt
 
 formatter = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename="knapsackRunner.log", level=logging.INFO, format=formatter)
@@ -12,7 +13,7 @@ data = np.load("./data.npz")
 X_1gtrain = data["X_1gtrain"]
 X_1gtest = data["X_1gtest"]
 
-y_map_function = lambda y: np.round(y / 75) + 1
+y_map_function = lambda y: np.round(y / 300) * 2 + 3
 
 y_train = y_map_function(data["y_train"])
 y_test = y_map_function(data["y_test"])
@@ -22,11 +23,11 @@ X_1gvalidation = X_1gtest[
 ]  # [ 611 0 6 27 7 99 3083.99 53.64 606.13] array of [int int int int int int float float float], 6x int 3x float -> these are the predictive features per item.
 y_validation = y_test[
     0:2880
-]  # [] floats, length 2880 -> this is the true WEIGHT value of the item. Values between 1 and 50
+]  # [] floats, length 2880 -> this is the true WEIGHT value of the item. Values around 3,5,7
 
 y_test = y_test[
     2880:
-]  # [] floats, length 8496 -> this is the true WEIGHT value of the item. Values between 1 and 50
+]  # [] floats, length 8496 -> this is the true WEIGHT value of the item. Values around 3,5,7
 X_1gtest = X_1gtest[
     2880:, :
 ]  # [ 611 0 6 27 7 99 3083.99 53.64 606.13] array of [int int int int int int float float float], 6x int 3x float -> these are the predictive features per item.
@@ -34,7 +35,13 @@ X_1gtest = X_1gtest[
 
 random_values = []
 for i in range(0, 48):
-    random_values.append(random.randint(1, 70))
+    chance = random.randint(0, 20)
+    if chance == 9 or chance == 10:
+        random_values.append(np.random.normal(4500, 500))
+    elif chance == 8:
+        random_values.append(np.random.normal(1000, 200))
+    else:
+        random_values.append(random.randint(0, 600))
 
 clf = MSE_Learner(
     values=random_values,
@@ -66,5 +73,5 @@ pdf = clf.fit(
 )
 print(pdf.head())
 
-# np.set_printoptions(suppress=True, threshold=1000000)
-# print(max(y_validation))
+np.set_printoptions(suppress=True, threshold=1000000)
+print(max(y_validation))
